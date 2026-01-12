aa19cbffb32fe2252f647aae5637a08bdd37409e
Peter Polidoro <peter@polidoro.io>
2026-01-12 11:32:48 -0500

    Reformat manifest

--- guix/manifest.scm
+++ guix/manifest.scm
@@ -1,59 +1,44 @@
-;; manifest.scm — poster toolchain for mouse-joystick-poster
-;;
-;; Enter with:
-;;   make shell
-;;   make shell-pure
-;;
-;; Notes:
-;; - Some GUI apps rely on host graphics drivers; Guix manages userland packages.
-;; - Fonts/locales/certificates are included because they commonly bite "pure" shells.
+(specifications->manifest
+ '(;; CAD / EDA / rendering
+   "freecad"
+   "blender"
+   "kicad"
+   "kicad-doc"
+   "kicad-symbols"
+   "kicad-footprints"
+   "kicad-packages3d"
+   "kicad-templates"
 
-(use-modules (guix packages)
-             (guix profiles))
+   ;; Layout / raster+vector
+   "inkscape"
+   "gimp"
 
-(packages->manifest
- (list
-  ;; CAD / EDA / rendering
-  freecad
-  blender
-  kicad
-  kicad-doc
-  kicad-symbols
-  kicad-footprints
-  kicad-packages3d
-  kicad-templates
+   ;; Conversions / export helpers
+   "imagemagick"
+   "ghostscript"
+   "poppler"
+   "ffmpeg"
+   "qrencode"
 
-  ;; Layout / raster+vector
-  inkscape
-  gimp
+   ;; Version control helpers
+   "git"
+   "git-lfs"
 
-  ;; Conversions / export helpers
-  imagemagick
-  ghostscript
-  poppler
-  ffmpeg
-  qrencode
+   ;; Common runtime needs for GUI apps in (some) Guix shells
+   "fontconfig"
+   "font-dejavu"
+   "font-gnu-freefont"
+   "font-ghostscript"
+   "glibc-locales"
+   "nss-certs"
 
-  ;; Version control helpers
-  git
-  git-lfs
-
-  ;; Common runtime needs for GUI apps in (some) Guix shells
-  fontconfig
-  font-dejavu
-  font-gnu-freefont
-  font-ghostscript
-  glibc-locales
-  nss-certs
-
-  ;; Quality-of-life tools
-  bash
-  coreutils
-  findutils
-  grep
-  sed
-  gawk
-  less
-  which
-  file
-  ))
+   ;; Quality-of-life tools
+   "bash"
+   "coreutils"
+   "findutils"
+   "grep"
+   "sed"
+   "gawk"
+   "less"
+   "which"
+   "file"))
