105ed8cc2f88a83c969f69c1e06e6698ca83c95a
Peter Polidoro <peter@polidoro.io>
2026-01-24 09:49:52 -0500

    Update channels

--- guix/channels.scm
+++ guix/channels.scm
@@ -3,7 +3,7 @@
         (url "https://git.guix.gnu.org/guix.git")
         (branch "master")
         (commit
-          "5dbab32115de3276f582bcb801fd9f88abe0d397")
+          "3a7ac72469e4061960b07439353ac3682ed93871")
         (introduction
           (make-channel-introduction
             "9edb3f66fd807b096b48283debdcddccfea34bad"
